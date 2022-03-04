85fb04add2d4909b958e54f857d5fc3c6e0d48ee
GB-Titi <tristanmatteoli@gmail.com>
2022-03-04 09:39:07 +0100

    ajout projet

---
+++ Back/tests/ApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiTest extends WebTestCase
+{
+    public function testApiGET(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('GET', '/api/');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['message' => "Hello world"], $responseData);
+    }
+
+    public function testApiPOST(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['message' => "Hello world"], $responseData);
+    }
+
+    public function testApiDELETE(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('DELETE', '/api/');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['message' => "Hello world"], $responseData);
+    }
+}
